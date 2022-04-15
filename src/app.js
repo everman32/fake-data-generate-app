57459c1d8fae59994f0b770de95e9c6461271b7b
Victor <vitek.borodyn@gmail.com>
2022-04-15 16:17:27 +0300

    first-commit

---
+++ src/app.js
@@ -0,0 +1,95 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Container, Form } from "react-bootstrap";
+import Button from "react-bootstrap/Button";
+import dataGenerator from "./data-generator";
+import Table from "./components/table" 
+
+const Styles = styled.div`
+  padding: 1rem;
+
+  table {
+    border-spacing: 0;
+    border: 1px solid black;
+
+    tr {
+      :last-child {
+        td {
+          border-bottom: 0;
+        }
+      }
+    }
+
+    th,
+    td {
+      margin: 0;
+      padding: 0.5rem;
+      border-bottom: 1px solid black;
+      border-right: 1px solid black;
+
+      :last-child {
+        border-right: 0;
+      }
+    }
+  }
+`;
+
+function App() {
+  const [seed, setSeed]=useState();
+  const [items, setItems] = useState(dataGenerator(40));
+
+
+  const columns = React.useMemo(
+    () => [
+
+      {
+        Header: "UUID",
+        accessor: "uuid"
+      },
+      {
+        Header: "Name",
+        accessor: "name"
+      },
+      {
+        Header: "Address",
+        accessor: "address"
+      },
+      {
+        Header: "Phone",
+        accessor: "phone"
+      }
+    ],
+    []
+  );
+
+  const fetchMoreData = () => {
+    setTimeout(() => {
+      setItems(items.concat(dataGenerator(20)));
+    }, 1500);
+  };
+
+  const data = React.useMemo(() => items, [items]);
+
+  return (
+    <Container>
+      <Form.Control
+        className="mt-3"
+        placeholder="Enter seed..."
+        value={seed}
+        onChange={e => setSeed(e.target.value)}
+      //type="password"
+      />
+      <Button
+      //variant={"outline-success"}
+      //onClick={singInButtonClick}
+      >
+        Set seed
+      </Button>
+      <Styles> 
+        <Table columns={columns} data={data} update={fetchMoreData} />
+      </Styles>
+    </Container>
+  );
+}
+
+export default App;
---
+++ src/data-generator.js
@@ -0,0 +1,36 @@
+const { faker } = require('@faker-js/faker');
+
+const range = len => {
+  const arr = [];
+  for (let i = 0; i < len; i++) {
+    arr.push(i);
+  }
+  return arr;
+};
+
+const newPerson = () => { 
+  return {
+    uuid: faker.datatype.uuid(),
+    name: faker.name.findName(),
+    address: [
+              faker.address.state(),
+              faker.address.cityName(),
+              faker.address.streetAddress(),
+              faker.address.secondaryAddress()].join(', '),
+    phone:faker.phone.phoneNumber()
+  };
+};
+
+export default function makeData(...lens) {
+  const makeDataLevel = (depth = 0) => {
+    const len = lens[depth];
+    return range(len).map(d => {
+      return {
+        ...newPerson(),
+        subRows: lens[depth + 1] ? makeDataLevel(depth + 1) : undefined
+      };
+    });
+  };
+
+  return makeDataLevel();
+}
