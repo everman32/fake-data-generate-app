85088947bcec2063ad65ac87831774f47e7d5f14
Victor <vitek.borodyn@gmail.com>
2023-03-08 21:53:35 +0300

    Replace magic numbers with constants

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import generateData from "./data-generator";
 import Table from "./components/table";
 import TableStyle from "./components/table-style";
 
+const INITIAL_RECORDS_COUNT = 40;
+const ADDITIONAL_RECORDS_COUNT = 20;
+
 const columns = [
   {
     Header: "UUID",
@@ -23,7 +26,7 @@ const columns = [
   },
 ];
 
-const initialData = generateData(40);
+const initialData = generateData(INITIAL_RECORDS_COUNT);
 
 function App() {
   const [items, setItems] = useState(initialData);
@@ -32,7 +35,10 @@ function App() {
   const getMoreData = useCallback(() => {
     setIsLoading(true);
     requestAnimationFrame(() => {
-      setItems((prevItems) => [...prevItems, ...generateData(20)]);
+      setItems((prevItems) => [
+        ...prevItems,
+        ...generateData(ADDITIONAL_RECORDS_COUNT),
+      ]);
       setIsLoading(false);
     });
   }, []);
