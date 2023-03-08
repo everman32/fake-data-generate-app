a2c33cd0136725b42057b2b873a66b2ba5295f8b
Victor <vitek.borodyn@gmail.com>
2023-03-08 04:51:26 +0300

    Refactor App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,49 @@
-import React, { useState } from "react";
+import React, { useState, useCallback, useMemo } from "react";
 import { Container } from "react-bootstrap";
 import generateData from "./data-generator";
 import Table from "./components/table";
 import TableStyle from "./components/table-style";
 
+const columns = [
+  {
+    Header: "UUID",
+    accessor: "uuid",
+  },
+  {
+    Header: "Name",
+    accessor: "name",
+  },
+  {
+    Header: "Address",
+    accessor: "address",
+  },
+  {
+    Header: "Phone",
+    accessor: "phone",
+  },
+];
+
+const initialData = generateData(40);
+
 function App() {
-  const [items, setItems] = useState(generateData(40));
-  const columns = React.useMemo(
-    () => [
-      {
-        Header: "UUID",
-        accessor: "uuid",
-      },
-      {
-        Header: "Name",
-        accessor: "name",
-      },
-      {
-        Header: "Address",
-        accessor: "address",
-      },
-      {
-        Header: "Phone",
-        accessor: "phone",
-      },
-    ],
-    []
-  );
+  const [items, setItems] = useState(initialData);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const getMoreData = () => {
-    setTimeout(() => {
-      setItems(items.concat(generateData(20)));
-    }, 1500);
-  };
+  const getMoreData = useCallback(() => {
+    setIsLoading(true);
+    requestAnimationFrame(() => {
+      setItems((prevItems) => [...prevItems, ...generateData(20)]);
+      setIsLoading(false);
+    });
+  }, []);
 
-  const data = React.useMemo(() => items, [items]);
+  const data = useMemo(() => items, [items]);
 
   return (
     <Container>
       <TableStyle>
         <Table columns={columns} data={data} update={getMoreData} />
+        {isLoading && <div>Loading...</div>}
       </TableStyle>
     </Container>
   );
